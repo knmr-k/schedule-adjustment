595fd99d5c8e729b68a39eac16bdf1db7f8b2975
knmr-k <kanamr.kz@gmail.com>
2023-09-20 16:54:57 +0900

    予定の取得をActive Recordメソッドを使用する方法に変更

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,19 +1,19 @@
 module SchedulesHelper
 
+  # 予定を表示する機能
   def plan_check(num, user)
-    plans = Plan.includes(:user)
     date = Date.today
     check_day = date.beginning_of_week + num
-    plan_search = plans.select{|i| (i.date == check_day) && (i.user_id == user.id)}
+    plan_search = Plan.find_by(date: check_day, user_id: user.id)
 
-    if plan_search == []
+    if plan_search == nil
       render inline: "<div class=\"plan-blank\">-</div>"
     elsif !user_signed_in?
       render inline: "<div class=\"plan-que\">✅</div>"
-    elsif (current_user.id == plan_search[0].user_id) || (current_user.admin == true)
-      if plan_search[0].plan == true
+    elsif (current_user.id == plan_search.user_id) || (current_user.admin == true)
+      if plan_search.plan == true
         render inline: "<div class=\"plan-ok\">⭕</div>"
-      elsif plan_search[0].plan == false
+      elsif plan_search.plan == false
         render inline: "<div class=\"plan-no\">❌</div>"
       end
     else
@@ -21,19 +21,19 @@ module SchedulesHelper
     end
   end
 
+  # 予定に応じた結果を表示する機能
   def plan_result(num)
-    plans = Plan.includes(:user)
     user_list = User.all
     date = Date.today
     check_day = date.beginning_of_week + num
     result = "<div class=\"plan-ok\">⭕</div>"
 
     user_list.each do |user|
-      plan_search = plans.select{|i| i.date == check_day && i.user_id == user.id}
-      if plan_search == []
+      plan_search = Plan.find_by(date: check_day, user_id: user.id)
+      if plan_search == nil
         result = "<div class=\"plan-blank\">❓</div>"
         break
-      elsif plan_search[0].plan == false
+      elsif plan_search.plan == false
         result = "<div class=\"plan-no\">❌</div>"
       end
     end
